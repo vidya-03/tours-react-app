c647f25f723b27834c9417e5da5f778c2b2445b9
vidya-03 <vidyashree581110@gmail.com>
2024-03-14 12:24:17 +0530

    added happy journey

--- src/components/Header.js
+++ src/components/Header.js
@@ -5,9 +5,9 @@ import React from 'react';
 const Header = ()=>{
 return(
  <div className='container'>
-    <div className='title'>Plan a Tour</div>
+    <div className='title'>Let's Go</div>
     
-  
+  <h1>Happy Journey</h1>
  </div>
 )
 };
